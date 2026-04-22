f026148b23b66e15821d0f40090b4ad3d2cf7e42
Erick Villarreal <evillarreal@hammondpowersolutions.com>
2026-04-22 17:02:35 -0600

    added features, excel export

--- src/core_al/nesting.py
+++ src/core_al/nesting.py
@@ -1,9 +1,36 @@
-
 def nesting_parts(parts, profiles):
     profiles_dict = {p.name: p for p in profiles}
-    available_parts = group_parts(parts)
+    grouped_parts = group_parts(parts)
+    nested = {}
+    for profile, parts_list in grouped_parts.items():
+        profile_length = profiles_dict[profile].length
+
+        parts_lengths = [p.length for p in parts_list]
+        parts_lengths.sort(reverse = True)
+
+        nest = nesting(profile_length, parts_lengths)
+        nested[profile] = {
+            "nestings": nest,
+            "profile_length": profile_length,
+        }
     
-    return available_parts
+    return nested
+        
+
+def nesting(profile_length, parts_lengths):
+    bars = []
+
+    for part in parts_lengths:
+        placed = False
+
+        for bar in bars:
+            if sum(bar) + part <= profile_length:
+                bar.append(part)
+                placed = True
+                break
+        if not placed:
+            bars.append([part])
+    return bars
 
 def group_parts(parts):
     available_parts = {}
--- src/main.py
+++ src/main.py
@@ -1,13 +1,33 @@
 from src.data_io.xls_reader import read_parts, read_profiles
 from src.core_al.nesting import nesting_parts
 from src.models.Parts import Part
+import pandas as pd
 
 excel_path = r"C:\Users\evillarreal\Documents\Personales\Programacion\python\projects\profile_nesting\pruebas.xlsx"
 parts_data = read_parts(excel_path)
 profiles_data = read_profiles(excel_path)
 
-resultado = nesting_parts(parts_data, profiles_data)
+final_data = nesting_parts(parts_data, profiles_data)
 
-# convertir profiles_data a diccionario para acceso rápido
-profiles_dict = {p.name: p for p in profiles_data}
+data_rows = []
+
+for profile, data in final_data.items():
+    profile_length = data["profile_length"]
+
+    for i, bar in enumerate(data["nestings"], 1):
+        used = sum(bar)
+        waste = profile_length - used
+
+        data_rows.append({
+            "profile": profile,
+            "bar": i,
+            "pieces": ", ".join(str(x) for x in bar),
+            "used": used,
+            "waste": waste
+        })
+
+data_frame = pd.DataFrame(data_rows)
+
+with pd.ExcelWriter(excel_path, mode="a", engine="openpyxl", if_sheet_exists="replace") as writer:
+    data_frame.to_excel(writer, sheet_name="Nesting_Result", index=False)
 
